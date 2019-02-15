87e00ecbed71995b9d9f3dfa5d38ebdd51282211
PyryV <pyry.vekkeli@helsinki.fi>
2019-02-15 17:49:02 +0200

    UI testit palautettu

--- src/test/java/labyrintti/ui/UITest.java
+++ src/test/java/labyrintti/ui/UITest.java
@@ -1,15 +1,23 @@
 package labyrintti.ui;
 
 import static org.junit.Assert.*;
+
+import java.util.Scanner;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import labyrintti.domain.Labyrintinluoja;
+
+
 public class UITest {
     public UI ui;
- /*
+ 
     @Before
     public void setUp() {
-        ui = new UI();
+        Scanner scanner = new Scanner(System.in);
+        Labyrintinluoja luoja = new Labyrintinluoja();
+        ui = new UI(luoja, scanner);
 
     }
 
@@ -20,7 +28,7 @@ public class UITest {
         assertEquals(1274, ui.piirra().length());
     }
 
-    */
+    
 
 
 }
